953bcda332b7f5b340dcbb18aed5f49d16734789
Sergei Merenkov <mer.sergei@gmail.com>
2013-08-14 12:20:47 +0300

    tst commit

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -5,11 +5,10 @@ class AppDelegate
     @window.rootViewController = @mainViewController 
     @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
-=begin   
+
  	alert = UIAlertView.new
     alert.message = "Hello World!"
     alert.show
-=end
 
 	true
   end
